b354e701208a23f83cf391fc6736fd58b5d0ba2f
Pedro Augusto <pedrogaviiao@gmail.com>
2023-05-22 00:31:35 -0300

    att

---
+++ script.js
@@ -0,0 +1,48 @@
+let searchBtn = document.getElementById("search-btn");
+let countryInp = document.getElementById("country-inp");
+let result = document.getElementById("result")
+
+searchBtn.addEventListener("click", () => {
+    let countryName = countryInp.value;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        result.innerHTML += `<img src="${data[0].flags.svg}"
+        class="flag-img">
+        <h2>${data[0].name.common}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${data[0].capital[0]}</span>
+            </div>
+        </div>
+
+    <div class="wrapper">
+        <div class="data-wrapper">
+            <h4>Continete:</h4>
+            <span>${data[0].continents[0]}</span>
+        </div>
+    </div>
+
+    <div class="wrapper">
+        <div class="data-wrapper">
+            <h4>População:</h4>
+            <span>${data[0].population[0]}</span>
+        </div>
+    </div>
+
+    <div class="wrapper">
+        <div class="data-wrapper">
+            <h4>Moeda:</h4>
+            <span>${data[0].currencies[Object.keys(data
+                [0].currencies)].name
+            } - ${Object.keys(data[0].currencies)
+                [0]}</span>
+        </div>
+    </div>
+
+        `
+    });
+})
